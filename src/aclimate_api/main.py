a2763fab5196056413cc480a2714499a1dacff36
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2024-05-28 14:12:13 -0500

    geoserver error and document

--- src/aclimate_api/main.py
+++ src/aclimate_api/main.py
@@ -7,7 +7,7 @@ from geoserver import Geoserver
 
 def main():
     g = Geographic("https://webapi.aclimate.org/api/")
-    print(g.get_geographic_ws("61e59d829d5d2486e18d2ea8"))
+    # print(g.get_geographic_ws("61e59d829d5d2486e18d2ea8"))
     f = Forecast("https://webapi.aclimate.org/api/")
     # print(f.get_forecast_crop_exc(["5a7e422057d7f316c8bc574e"]))
     a = Agronomy("https://webapi.aclimate.org/api/")
@@ -15,7 +15,7 @@ def main():
     h = Historical("https://webapi.aclimate.org/api/")
     # print(h.get_historical_climatology(["5a7e422057d7f316c8bc574e"]))
     gs = Geoserver("https://geo.aclimate.org/geoserver/")
-    # print(gs.get_geo_workspaces())
+    # print(gs.get_geo_polygon_name("fc_cenaos_hn"))
 
 if __name__ == "__main__":
     main()
