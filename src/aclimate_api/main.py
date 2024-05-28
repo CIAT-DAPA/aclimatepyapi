4ec43eb470f6664259ecc01e3b7f260c2c7d1ba5
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2024-05-28 13:58:02 -0500

    geographic error and document

--- src/aclimate_api/main.py
+++ src/aclimate_api/main.py
@@ -7,9 +7,9 @@ from geoserver import Geoserver
 
 def main():
     g = Geographic("https://webapi.aclimate.org/api/")
-    # print(g.get_geographic("636c0813e57f2e6ac61394e6"))
+    print(g.get_geographic_ws("61e59d829d5d2486e18d2ea8"))
     f = Forecast("https://webapi.aclimate.org/api/")
-    print(f.get_forecast_crop_exc(["5a7e422057d7f316c8bc574e"]))
+    # print(f.get_forecast_crop_exc(["5a7e422057d7f316c8bc574e"]))
     a = Agronomy("https://webapi.aclimate.org/api/")
     # print(a.get_agronomy())
     h = Historical("https://webapi.aclimate.org/api/")
